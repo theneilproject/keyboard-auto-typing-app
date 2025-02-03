260e8ac447598803ae87ce006b22e488143395f0
Neil <152786225+theneilproject@users.noreply.github.com>
2025-02-03 11:43:11 +0900

    기본 환경 세팅 및 플러그인 설치 완료

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,32 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+async fn read_binary_file(path: String) -> Result<Vec<u8>, String> {
+    std::fs::read(path).map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+async fn write_binary_file(path: String, data: Vec<u8>) -> Result<(), String> {
+    std::fs::write(path, data).map_err(|e| e.to_string())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            read_binary_file,
+            write_binary_file
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
