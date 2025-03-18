dcdb8988aafc55bf83027f945691425b0ecf468a
Neil <152786225+theneilproject@users.noreply.github.com>
2025-03-18 12:53:27 +0900

    - dotnet database tool update also added

--- src/store/dotnet.js
+++ src/store/dotnet.js
@@ -10,9 +10,27 @@
       },
       {
         id: 3122,
-        name: 'update',
+        name: 'migration update',
         command: 'dotnet ef database update ^{migrationName} --project ^{projectName} --context ^{contextName}',
         detail: '데이터베이스를 업데이트합니다.'
+      },
+      {
+        id: 3123,
+        name: 'migration remove',
+        command: 'dotnet ef migrations remove --project ^{projectName} --context ^{contextName}',
+        detail: '마이그레이션을 삭제합니다.'
+      },
+      {
+        id: 3124,
+        name: 'migration list',
+        command: 'dotnet ef migrations list --project ^{projectName} --context ^{contextName}',
+        detail: '마이그레이션 목록을 확인합니다.'
+      },
+      {
+        id: 3125,
+        name: 'tool update',
+        command: 'dotnet tool update --global dotnet-ef --version ^{version}',
+        detail: 'dotnet-ef 도구를 업데이트합니다.'
       }
     ]
   }
